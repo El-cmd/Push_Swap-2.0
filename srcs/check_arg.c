f438908c8516102af237a04633817dec1acb4d65
Emma NGO <engo@e2r1p4.clusters.42paris.fr>
2022-09-19 14:05:11 +0200

    check arg

---
+++ srcs/check_arg.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_arg.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: engo <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 01:58:32 by engo              #+#    #+#             */
+/*   Updated: 2022/09/16 04:16:03 by engo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	check_isdigit(char *arg)
+{
+	int	i;
+
+	i = 0;
+	if (arg[i] == '-')
+		i++;
+	while (arg[i])
+	{
+		if (!(ft_isdigit(arg[i])))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_isint(char *arg)
+{
+	long int	res;
+
+	res = ft_atoi(arg);
+	if (res > 2147483647 || res < -2147483648)
+		return (1);
+	return (0);
+}
+
+int	check_double(char **str, int i)
+{
+	int	j;
+
+	j = i + 1;
+	while (str[j])
+	{
+		if (ft_atoi(str[j]) == ft_atoi(str[i]))
+			return (1);
+		j++;
+	}
+	return (0);
+}
+
+void	check_arg(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		if (check_isdigit(tab[i]) == 1 || check_isint(tab[i]) == 1
+			|| check_double(tab, i == 1))
+		{
+			free_tab(tab);
+			ft_putstr("Error\n");
+			exit(2);
+		}
+		i++;
+	}
+	return ;
+}
